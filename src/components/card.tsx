aa9619b0482aa45e2a18b2ac9934d4611a2a719c
roshni-patel <rpatel01@wesleyan.edu>
2021-09-04 23:26:08 -0500

    Add url prop to card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,19 +1,21 @@
 import React from "react";
+import './card.css'
 
 type CardProps = {
     img: string, 
     altText: string, 
     title: string, 
-    description: string
+    description: string,
+    url: string
 }
 
-const Card: React.FC<CardProps> = ({ img, altText, title, description }: CardProps) => {
+const Card: React.FC<CardProps> = ({ img, altText, title, description, url }: CardProps) => {
     return(
         <div className="col d-flex justify-content-center">
             <div className="card m-4 min-vw-50">
-                <img src={img} className="card-img-top" alt={altText} />
+                <a href={url}><img src={img} className="card-img-top" alt={altText} /></a>
                 <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
+                    <a href={url}><h5 className="card-title">{title}</h5></a>
                     <p className="card-text">{description}</p>
                 </div>
             </div>
--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -6,27 +6,32 @@ const projects = [
     {
         title: "Project 1",
         description: "This is the description for project 1",
-        img: "https://images.unsplash.com/photo-1551721434-8b94ddff0e6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80"
+        img: "https://images.unsplash.com/photo-1551721434-8b94ddff0e6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80",
+        url: "https://github.com/"
     },
     {
         title: "Project 2",
         description: "This is the description for project 2",
-        img: "https://images.unsplash.com/photo-1551721434-8b94ddff0e6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80"
+        img: "https://images.unsplash.com/photo-1551721434-8b94ddff0e6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80",
+        url: "https://github.com/"
     }, 
     {
         title: "Project 3",
         description: "This is the description for project 3",
-        img: "https://images.unsplash.com/photo-1551721434-8b94ddff0e6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80"
+        img: "https://images.unsplash.com/photo-1551721434-8b94ddff0e6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80",
+        url: "https://github.com/"
     },
     {
         title: "Project 4",
         description: "This is the description for project 4",
-        img: "https://images.unsplash.com/photo-1551721434-8b94ddff0e6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80"
+        img: "https://images.unsplash.com/photo-1551721434-8b94ddff0e6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80",
+        url: "https://github.com/"
     },
     {
         title: "Project 5",
         description: "This is the description for project 5",
-        img: "https://images.unsplash.com/photo-1551721434-8b94ddff0e6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80"
+        img: "https://images.unsplash.com/photo-1551721434-8b94ddff0e6d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1001&q=80",
+        url: "https://github.com/"
     }
 ]
 
@@ -38,7 +43,7 @@ const Portfolio: React.FC = () => {
             <div className="row">
                 {projects.map((project) => {
                     return(
-                        <Card img={project.img} altText={"project image"} title={project.title} description={project.description} />
+                        <Card img={project.img} altText={"project image"} title={project.title} description={project.description} url={project.url} />
                     )})
                 }
             </div> 
