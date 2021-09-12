7cec1ff13198300e5602560e4a645892cb00d3ff
roshni-patel <rpatel01@wesleyan.edu>
2021-09-12 14:08:07 -0500

    Setup modal for add project form

--- src/components/addprojectform.tsx
+++ src/components/addprojectform.tsx
@@ -55,44 +55,56 @@ const AddProjectForm: React.FC = () => {
     };
 
     return(
-        <div className="row portfolio__add__project__form__container">
-            <section className="portfolio__add__project__form col-lg-6 col-md-6 d-flex shadow-lg p-3 mb-5 bg-white rounded">
-                <form onSubmit={onFormSubmit}>
-                    <div className="row d-flex">
-                        <div className="form-group col-md-6 col-sm-12 p-2">
-                            <label htmlFor="title">Title</label>
-                            <input type="text" name="title" value={formFields.title} onChange={onFormFieldChange} className="form-control" placeholder="The title of your project" data-rule="minlen:1" data-msg="Please enter a title"/>
-                            <div className="validate"></div>
-                        </div>
-                        <div className="form-group col-md-6 col-sm-12 p-2">
-                            <label htmlFor="img">Image</label>
-                            <input type="text" name="img" value={formFields.img} onChange={onFormFieldChange} className="form-control" placeholder="A link to an image for your project" data-rule="minlen:1" data-msg="Please enter a link to an image"/>
-                            <div className="validate"></div>
-                        </div>
-                        <div className="form-group col-md-6 col-sm-12 p-2">
-                            <label htmlFor="altText">Image Alt Text</label>
-                            <input type="text" name="altText" value={formFields.altText} onChange={onFormFieldChange} className="form-control" placeholder="Alt text for your project's image" data-rule="minlen:1" data-msg="Please enter alt text for the project image" />
-                            <div className="validate"></div>
-                        </div>
-                        <div className="form-group col-md-6 col-sm-12 p-2">
-                            <label htmlFor="url">Link</label>
-                            <input type="text" name="url" value={formFields.url} onChange={onFormFieldChange} className="form-control" placeholder="A link to your project" data-rule="minlen:1" data-msg="Please enter a link to the project" />
-                            <div className="validate"></div>
-                        </div>
-                        <div className="form-group col-md-6 col-sm-12 p-2">
-                            <label htmlFor="description">Description</label>
-                            <textarea name="description" value={formFields.description} onChange={onFormFieldChange} className="form-control" placeholder="A description of your project" data-rule="minlen:1" data-msg="Please enter a description" />
-                            <div className="validate"></div>
-                        </div>
-                        <div className="form-row col-medium-12 d-flex align-items-stretch pt-3">
-                            <div className="form-group col-md-12">
-                                <button onClick={onFormSubmit} className="btn btn-primary btn-md">Add Project</button>
-                            </div>
-                        </div>
+        <>
+        <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Project Details</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                </form>
-            </section>
-        </div>
+                    <div className="modal-body">
+                        <section className="portfolio__add__project__form col-lg-6 col-md-6 d-flex shadow-lg p-3 mb-5 bg-white rounded">
+                            <form onSubmit={onFormSubmit}>
+                                <div className="row d-flex">
+                                    <div className="form-group col-md-6 col-sm-12 p-2">
+                                        <label htmlFor="title">Title</label>
+                                        <input type="text" name="title" value={formFields.title} onChange={onFormFieldChange} className="form-control" placeholder="The title of your project" data-rule="minlen:1" data-msg="Please enter a title"/>
+                                        <div className="validate"></div>
+                                    </div>
+                                    <div className="form-group col-md-6 col-sm-12 p-2">
+                                        <label htmlFor="img">Image</label>
+                                        <input type="text" name="img" value={formFields.img} onChange={onFormFieldChange} className="form-control" placeholder="A link to an image for your project" data-rule="minlen:1" data-msg="Please enter a link to an image"/>
+                                        <div className="validate"></div>
+                                    </div>
+                                    <div className="form-group col-md-6 col-sm-12 p-2">
+                                        <label htmlFor="altText">Image Alt Text</label>
+                                        <input type="text" name="altText" value={formFields.altText} onChange={onFormFieldChange} className="form-control" placeholder="Alt text for your project's image" data-rule="minlen:1" data-msg="Please enter alt text for the project image" />
+                                        <div className="validate"></div>
+                                    </div>
+                                    <div className="form-group col-md-6 col-sm-12 p-2">
+                                        <label htmlFor="url">Link</label>
+                                        <input type="text" name="url" value={formFields.url} onChange={onFormFieldChange} className="form-control" placeholder="A link to your project" data-rule="minlen:1" data-msg="Please enter a link to the project" />
+                                        <div className="validate"></div>
+                                    </div>
+                                    <div className="form-group col-md-6 col-sm-12 p-2">
+                                        <label htmlFor="description">Description</label>
+                                        <textarea name="description" value={formFields.description} onChange={onFormFieldChange} className="form-control" placeholder="A description of your project" data-rule="minlen:1" data-msg="Please enter a description" />
+                                        <div className="validate"></div>
+                                    </div>
+                                </div>
+                            </form>
+                        </section>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" onClick={onFormSubmit} className="btn btn-primary">Add Project</button>
+                    </div>
+                </div>
+                </div>
+                <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"><i className="fas fa-plus"></i></button>
+            </div>
+        </>
     )
 }
 
--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState, FormEvent } from 'react';
 import './portfolio.css'
 import Card from './card';
-import AddProjectForm from "./addprojectform";
 
 const projects = [
     {
@@ -42,7 +41,46 @@ const projects = [
 ]
 
 const Portfolio: React.FC = () => {
+    const [formFields, setFormFields] = useState({
+        title: '',
+        img: '',
+        altText: '',
+        description: '',
+        url: ''
+    })
+
+    const onFormFieldChange = (event: React.FormEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+        const newFormData = {
+            ...formFields 
+        };
+    
+        switch (event.currentTarget.name) {
+            case 'title': 
+            case 'img':
+            case 'altText':
+            case 'description':
+            case 'url': 
+                newFormData[event.currentTarget.name] = event.currentTarget.value;
+        }
+
+        setFormFields(newFormData); 
+    };
+
+    const onFormSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        
+        setFormFields({
+            title: '',
+            img: '',
+            altText: '',
+            description: '',
+            url: ''
+        });
+    };
+
+
     return(
+        <>
         <div className="background">
             <h2>Portfolio</h2>
             <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
@@ -52,9 +90,57 @@ const Portfolio: React.FC = () => {
                         <Card img={project.img} altText={"project image"} title={project.title} description={project.description} url={project.url} />
                     )})
                 }
-            </div> 
-            <AddProjectForm />
-        </div>
+            </div>
+            <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Project Details</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <section className="portfolio__add__project__form col-lg-6 col-md-6 d-flex shadow-lg p-3 mb-5 bg-white rounded">
+                            <form onSubmit={onFormSubmit}>
+                                <div className="row d-flex">
+                                    <div className="form-group col-md-6 col-sm-12 p-2">
+                                        <label htmlFor="title">Title</label>
+                                        <input type="text" name="title" value={formFields.title} onChange={onFormFieldChange} className="form-control" placeholder="The title of your project" data-rule="minlen:1" data-msg="Please enter a title"/>
+                                        <div className="validate"></div>
+                                    </div>
+                                    <div className="form-group col-md-6 col-sm-12 p-2">
+                                        <label htmlFor="img">Image</label>
+                                        <input type="text" name="img" value={formFields.img} onChange={onFormFieldChange} className="form-control" placeholder="A link to an image for your project" data-rule="minlen:1" data-msg="Please enter a link to an image"/>
+                                        <div className="validate"></div>
+                                    </div>
+                                    <div className="form-group col-md-6 col-sm-12 p-2">
+                                        <label htmlFor="altText">Image Alt Text</label>
+                                        <input type="text" name="altText" value={formFields.altText} onChange={onFormFieldChange} className="form-control" placeholder="Alt text for your project's image" data-rule="minlen:1" data-msg="Please enter alt text for the project image" />
+                                        <div className="validate"></div>
+                                    </div>
+                                    <div className="form-group col-md-6 col-sm-12 p-2">
+                                        <label htmlFor="url">Link</label>
+                                        <input type="text" name="url" value={formFields.url} onChange={onFormFieldChange} className="form-control" placeholder="A link to your project" data-rule="minlen:1" data-msg="Please enter a link to the project" />
+                                        <div className="validate"></div>
+                                    </div>
+                                    <div className="form-group col-md-6 col-sm-12 p-2">
+                                        <label htmlFor="description">Description</label>
+                                        <textarea name="description" value={formFields.description} onChange={onFormFieldChange} className="form-control" placeholder="A description of your project" data-rule="minlen:1" data-msg="Please enter a description" />
+                                        <div className="validate"></div>
+                                    </div>
+                                </div>
+                            </form>
+                        </section>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" onClick={onFormSubmit} className="btn btn-primary">Add Project</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
+                <button type="button" className="btn btn-primary ms-2" data-bs-toggle="modal" data-bs-target="#exampleModal"><i className="fas fa-plus"></i></button>
+            </div>
+        </>
     )
 }
 
