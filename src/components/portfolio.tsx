5f8ca034c8fbc73c235f23ec0a84ac6a6c357789
roshni-patel <rpatel01@wesleyan.edu>
2021-09-18 22:33:24 -0500

    Add smooth scrolling

--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -9,7 +9,7 @@ const Portfolio: React.FC = () => {
     
     return(
         <React.Fragment>
-            <div className="background">
+            <div className="background" id="portfolio">
                 <h2>Portfolio</h2>
                 <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
                 <div className="row">
