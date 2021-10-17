0577345798d68dbc6724ca8329ec6bdc8e58c653
Chris McAnally <chris@adadevelopersacademy.org>
2021-10-16 20:15:02 -0700

    update card to call delete callback

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,17 +2,26 @@ import React from "react";
 import './card.css'
 
 type CardProps = {
+    id: string | undefined,
     img: string, 
     altText: string, 
     title: string, 
     description: string,
-    url: string
+    url: string,
+    deleteCallback?: (str: string) => void,
 }
 
-const Card: React.FC<CardProps> = ({ img, altText, title, description, url }: CardProps) => {
+const Card: React.FC<CardProps> = ({ id, img, altText, title, description, url, deleteCallback }: CardProps) => {
     return(
         <div className="col d-flex justify-content-center">
             <div className="card m-4 min-vw-50">
+                { deleteCallback && id ? <button
+                     type="button"
+                     onClick={() => deleteCallback(id)}
+                     className="btn-close" 
+                     aria-label="Close">                    
+                     </button> : '' 
+                }
                 <a href={url} target="_blank" rel="noreferrer"><img src={img} className="card-img-top" alt={altText} /></a>
                 <div className="card-body">
                     <a href={url} target="_blank" rel="noreferrer"><h5 className="card-title">{title} <i className="fas fa-external-link-alt"></i></h5></a>
