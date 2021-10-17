f97d2c1d2c192b0a753de6593f6b51b7af5b88bb
Chris McAnally <chris@adadevelopersacademy.org>
2021-10-16 20:16:49 -0700

    fix imports for useFirebase

--- src/firebase-auth/useFirebase.tsx
+++ src/firebase-auth/useFirebase.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
 import { FireBaseConfigType } from '../config/firebase_config_type';
 import { initializeApp, getApps } from 'firebase/app';
+import { Firestore, getFirestore } from 'firebase/firestore';
 
-const useFirebase = (config: FireBaseConfigType, name: string) => {
+
+const useFirebase = (config: FireBaseConfigType, name = 'app') => {
   const [app, setApp] = useState(() => { 
     const app = getApps().find((app) => app.name === name)
       || initializeApp(config, name) 
